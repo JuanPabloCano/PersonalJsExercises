5db990f143d9b4f6fc6a28fe6589d6abfac92fdf
Juan Pablo Cano <canojuan0415@gmail.com>
2022-12-02 16:02:05 -0500

    Feat: added adventjs 2022 challenge #2

--- exercises/AdventJSExercises.js
+++ exercises/AdventJSExercises.js
@@ -114,3 +114,34 @@ const wrapped = wrapping(gifts);
 const wrappedMidu = wrappingMidu(gifts);
 console.log(wrapped);
 console.log(wrappedMidu);
+
+/**Un millonario ha comprado una red social y no trae buenas noticias. Ha anunciado que cada vez que una jornada de trabajo se pierde por un día festivo, habrá que compensarlo con dos horas extra otro día de ese mismo año.
+
+Obviamente la gente que trabaja en la empresa no le ha hecho ni pizca de gracia y están preparando un programa que les diga el número de horas extras que harían en el año si se aplicara la nueva norma.
+
+Al ser trabajo de oficina, su horario laboral es de lunes a viernes. Así que sólo tienes que preocuparte de los días festivos que caen en esos días.
+
+Dado un año y un array con las fechas de los días festivos, devuelve el número de horas extra que se harían ese año:
+
+const year = 2022
+const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
+
+// 01/06 es el 6 de enero, jueves. Cuenta.
+// 04/01 es el 1 de abril, un viernes. Cuenta.
+// 12/25 es el 25 de diciembre, un domingo. No cuenta.
+ */
+
+function countHours(year, holidays) {
+	let hourCounter = 0;
+	const mappedHolidays = holidays.forEach((holiday) => {
+		const day = new Date(`${holiday}/${year}`).getDay();
+		if (day >= 1 && day < 6) {
+			hourCounter += 2;
+		}
+	});
+	return hourCounter;
+}
+
+const year = 2022;
+const holidays = ['01/06', '04/01', '12/25'];
+countHours(year, holidays);
