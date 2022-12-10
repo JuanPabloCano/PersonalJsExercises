3ef9bfd2e09fb3823c56b4dc1d7734ac3dad7b51
Juan Pablo Cano <canojuan0415@gmail.com>
2022-12-10 18:00:49 -0500

    Feat: added adventjs exercise

--- exercises/AdventJSExercises.js
+++ exercises/AdventJSExercises.js
@@ -133,7 +133,7 @@ const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
 
 function countHours(year, holidays) {
 	let hourCounter = 0;
-	const mappedHolidays = holidays.forEach((holiday) => {
+	holidays.forEach((holiday) => {
 		const day = new Date(`${holiday}/${year}`).getDay();
 		if (day >= 1 && day < 6) {
 			hourCounter += 2;
@@ -157,3 +157,25 @@ countries.forEach((value, index, arr) => {
 		}
 	});
 });
+
+/**
+ * Tienes una caja de regalos de Navidad que Santa Claus quiere entregar a los niños. Cada regalo está representado por una cadena. Santa Claus tiene un trineo que puede llevar un peso limitado, y cada regalo dentro de la caja tiene un peso que es igual al número de letras en el nombre del regalo.
+
+Santa Claus también tiene una lista de renos que pueden ayudarlo a entregar los regalos. Cada reno tiene un límite de peso máximo que puede llevar. El límite de peso máximo de cada reno es igual a dos veces el número de letras en su nombre.
+
+Tu tarea es implementar una función distributeGifts(packOfGifts, reindeers) que recibe una caja de regalos y una lista de renos y devuelve el número máximo de cajas de estos regalos que Santa Claus puede entregar a los niños. Las cajas de regalos no se pueden dividir.
+ */
+
+function distributeGifts(packOfGifts, reindeers) {
+	const giftPackSize = packOfGifts.reduce((acc, curr) => acc + curr.length, 0);
+	const reindeersVolumetoCarry = reindeers.reduce(
+		(acc, curr) => acc + curr.length * 2,
+		0
+	);
+	return Math.floor(reindeersVolumetoCarry / giftPackSize);
+}
+
+const packOfGifts = ['book', 'doll', 'ball'];
+const reindeers = ['dasher', 'dancer'];
+
+distributeGifts(packOfGifts, reindeers);
