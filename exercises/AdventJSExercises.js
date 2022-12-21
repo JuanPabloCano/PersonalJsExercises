0cd7410191eea5a124a5940940cb9673c7a6a5db
Juan Pablo Cano <canojuan0415@gmail.com>
2022-12-21 11:37:51 -0500

    Feat: adventjs challenge #4 added

--- exercises/AdventJSExercises.js
+++ exercises/AdventJSExercises.js
@@ -185,9 +185,34 @@ distributeGifts(packOfGifts, reindeers);
 const isPalindrome = (words) => {
 	words.forEach((word) => {
 		const isWordPalindrome = word.split('').reverse().join('');
-		if (isWordPalindrome.toLowerCase() === word.toLowerCase()) console.log(`${word} is palindrome`);
+		if (isWordPalindrome.toLowerCase() === word.toLowerCase())
+			console.log(`${word} is palindrome`);
 		else console.log(`${word} is not palindrome`);
 	});
 };
 
-isPalindrome(['Home', 'HomePage', 'Anna', 'Bird', 'Deed', 'Kayak', 'Word'])
+isPalindrome(['Home', 'HomePage', 'Anna', 'Bird', 'Deed', 'Kayak', 'Word']);
+
+const obj = [
+	{ l: 1, w: 1, h: 1 },
+	{ l: 3, w: 3, h: 3 },
+	{ l: 2, w: 2, h: 2 },
+];
+
+/** Santa Claus necesita hacer una revisión de sus cajas de regalos para asegurarse de que puede empaquetarlas todas en su trineo. Cuenta con una serie de cajas de diferentes tamaños, que se caracterizan por su longitud, anchura y altura.
+
+Tu tarea es escribir una función que, dada una lista de cajas con sus tamaños, determine si es posible empaquetar todas las cajas en una sola, de manera que cada caja contenga a otra (que a su vez contenga a otra, y así sucesivamente).
+
+Cada caja representa sus medidas con un objeto. Por ejemplo: {l: 2, w: 3, h: 2}. Esto significa que la caja tiene una longitud de 2, una anchura de 3 y una altura de 2.
+
+Una caja entra en otra caja si todos los lados de la primera son menores a los lados de la segunda. Los elfos nos han dicho que las cajas no se pueden rotar, así que no se puede poner una caja de 2x3x2 en una caja de 3x2x2 */
+
+function fitsInOneBox(boxes) {
+	return boxes
+		.sort((a, b) => b.l + b.w + b.h - (a.l + a.w + a.h))
+		.slice(1)
+		.every(
+			(box, i) => box.l < boxes[i].l && box.w < boxes[i].w && box.h < boxes[i].h
+		);
+}
+console.log(fitsInOneBox(obj));
